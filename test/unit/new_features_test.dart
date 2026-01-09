12452e900cdc1bcc52a061a480dce6ff7efc678f
JTM Developer <dev@jtm.com>
2026-01-09 13:50:02 +0000

    feat: add deploy workflow and new features test template

---
+++ test/unit/new_features_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('New Features Tests', () {
+    test('should test new functionality', () {
+      // Ajoutez vos tests ici quand vous développez
+      expect(true, isTrue);
+    });
+  });
+}
